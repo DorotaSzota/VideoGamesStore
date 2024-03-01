c0003f3a34979798e61e5d761ba242bb520d9d11
Dorota <dorota.szota@edu.uekat.pl>
2024-03-01 09:09:01 +0100

    added create page

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VideoGamesStore.Data;
+using VideoGamesStore.Models;
+
 
 namespace VideoGamesStore.Controllers;
 
 public class CategoryController : Controller
 {
+    
     private readonly VgsDbContext _dbContext;
-    public CategoryController(VgsDbContext dbContext)
+    public CategoryController( VgsDbContext dbContext)
     {
         _dbContext = dbContext;
     }
@@ -15,4 +18,16 @@ public class CategoryController : Controller
         var objCategoryList = _dbContext.Categories.ToList();
         return View(objCategoryList);
     }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    /*[HttpPost]
+    public async Task<ActionResult> Create(CreateCategoryDto newCategory)
+    {
+        
+        return Ok();
+    }*/
 }
